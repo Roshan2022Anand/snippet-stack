f560df7ced8326e68da9d17c85e72e880bd83572
unknown <roshananand2005@gmail.com>
2025-01-12 12:14:36 +0530

    removed the frontend writter in next.js

--- FrontEnd/src/lib/server-utils.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from 'next/headers';
-
-//function to get the cookies
-export const getCookies = async () => {
-  const sessionObj = (await cookies()).get('session');
-  if (!sessionObj) return null;
-  const session = `${sessionObj.name}=${sessionObj.value}`;
-  return session;
-};
